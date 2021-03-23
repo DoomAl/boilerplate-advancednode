94ebd8f936403e767e94b0426b47d489044510c2
DoomAl1 <alvaromonferrer11@gmail.com>
2021-03-23 16:12:47 +0000

    #3

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
+const ObjectID = require('mongodb').ObjectID;
 
 const app = express();
 app.set('view engine', 'pug');
@@ -18,4 +21,24 @@ app.route('/').get((req, res) => {
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Listening on port ' + process.env.PORT);
+});
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, null);
+  });
 });
